1bd7210047119bab478a8362298a5b1f79d934a0
Sean Karlage <sean.karlage@gmail.com>
2015-04-15 14:52:05 -0400

    Changed some comments

--- knnclassifier.py
+++ knnclassifier.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import sys
 import numpy as np
 import math
@@ -7,19 +5,6 @@ import cv2
 
 from read_data import read_toplevel_dir
 from matplotlib import pyplot as plt
-
-# Image classes are divided into 24/25 letters, with:
-#     - Capitals (24 letters)
-#           - Normal
-#           - Suffix
-#     - Lowercase (25 letters)
-#           - Normal
-#           - Suffix
-#
-# Because there aren't an equal number of pixels or images in each dir in data,
-# Need to pad numpy arrays with zeros in read_data
-
-# XXX Only focus on capital, normal letters for now
 
 def divide_array(array, trainratio=0.5):
     divpoint = int( math.floor(array.shape[0] * trainratio) )
@@ -34,8 +19,6 @@ def main():
     indir = sys.argv[1]
 
     # Read in data to giant numpy array
-    # raw_data dimensions:
-    #     (#letters, #samples, imgheight, imgwidth)
     raw_data, labels = read_toplevel_dir(indir)
 
     # Set training and testing data/labels
@@ -47,7 +30,7 @@ def main():
     knn.train(train_data, train_labels)
 
     # Testing
-    k = range(1, 21)
+    k = range(4, 21)
     accuracies = []
     for i in k:
         ret, result, neighbors, dist = knn.find_nearest(test_data, k=i)
@@ -63,37 +46,5 @@ def main():
     plt.xlabel('k')
     plt.ylabel('Accuracy (%)')
     plt.show()
-
-#imgs = read_image_dir(indir)
-#
-#img = cv2.imread('digits.png')
-#gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#
-## Now we split the image to 5000 cells, each 20x20 size
-#cells = [np.hsplit(row,100) for row in np.vsplit(gray,50)]
-#
-## Make it into a Numpy array. It size will be (50,100,20,20)
-#x = np.array(cells)
-#
-## Now we prepare train_data and test_data.
-#train = x[:,:50].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-#test = x[:,50:100].reshape(-1,400).astype(np.float32) # Size = (2500,400)
-#
-## Create labels for train and test data
-#k = np.arange(10)
-#train_labels = np.repeat(k,250)[:,np.newaxis]
-#test_labels = train_labels.copy()
-#
-## Initiate kNN, train the data, then test it with test data for k=1
-#knn = cv2.KNearest()
-#knn.train(train,train_labels)
-#ret,result,neighbours,dist = knn.find_nearest(test,k=5)
-#
-## Now we check the accuracy of classification
-## For that, compare the result with test_labels and check which are wrong
-#matches = result==test_labels
-#correct = np.count_nonzero(matches)
-#accuracy = correct*100.0/result.size
-#print accuracy
 
 main()
